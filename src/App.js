0cd74682a77aca982b68a3f79560eb34518a25a4
Sebastian Perez-Delgado <46662157+SebastianPD@users.noreply.github.com>
2022-03-12 14:46:24 -0600

    updated jest tests

--- src/App.js
+++ src/App.js
@@ -155,6 +155,7 @@ export default function App() {
     var duration;
 
     console.log(totaltime);
+    console.log(user);
 
     const success = async (position) => {
       const currentPosition = {
--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,41 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { useData, useUserState }from './utilities/firebase.js';
 
+
 jest.mock('./utilities/firebase.js');
 const mdata = {
-  "users":{"displayName":"Alex"},
+  "users":{
+    "displayName":"Alex"},
   "Locations":{},
 };
-const muser = {"users": {"name":"Alex"},
-                "user": {"displayName":"Alex"},};
+
+const muser = {
+  "Locations" : [null],
+  "title" : "All Locations",
+  "users" :  { 
+  "0bMtDcAIKyUkTFK0jWDgW6Vmlnt1" : {
+  "info" : {
+    "Homelatlng" : {
+      "lat" : 41.921634,
+      "lng" : -87.659558
+    },
+    "address" : "",
+    "email" : "c-riesbeck@u.northwestern.edu",
+    "img" : "https://lh3.googleusercontent.com/a-/AOh14Gid_efaV6PYa1_fc8Fkeg9jO7bz0U5j9Bij3LCs=s96-c",
+    "name" : "Christopher Riesbeck",
+    "payment" : "",
+    "petname" : ""
+  }
+}}};
+
+const muser2 = {
+  "accessToken" : "",
+  "auth" : [null],
+  "displayName" : "Christopher Riesbeck",
+  "email" : "c-riesbeck@u.northwestern.edu",
+  "emailVerified" : "true",
+  "isAnonymous" : "false",
+ };
 
 test('renders learn react link', async () => {
   useData.mockReturnValue([mdata, false, null]);
@@ -18,7 +46,7 @@ test('renders learn react link', async () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', async () => {
+test('Get unlock', async () => {
   useData.mockReturnValue([mdata, false, null]);
   useUserState.mockReturnValue(muser);
   render(<App />);
@@ -37,10 +65,11 @@ it('shows Sign Out if logged in', async () => {
 });
 
 
-it('shows if user photo is accessible', async () => {
-  useData.mockReturnValue([mdata, false, null]);
-  useUserState.mockReturnValue(muser);
+it('shows if user email is displayed', async () => {
+  useData.mockReturnValue([muser, false, null]);
+  useUserState.mockReturnValue(muser2);
   render(<App />);
-  const button = screen.getByAltText("userphoto");
+
+  const button = await screen.getByText("c-riesbeck@u.northwestern.edu");
   expect(button).toBeInTheDocument();
 });
